0b9ca37dc81d3f73e8f58caee2edb3c2cb10e125
Mago Yusuf <magonsubuga21@gmail.com>
2021-05-31 18:04:23 +0300

    practice sessions

---
+++ data-types/funtions.js/obb.js
@@ -0,0 +1,21 @@
+//objects
+//we start looking at software as objects
+//objects have characteristics
+//objects can be inherited 
+//an object is an instance of a class. a class is a blue print of an object
+//abstruction the top most representation of a class
+//encapsulation: the ability of class or object to hide its implimentation
+
+var mountain = {
+    name: "rwenzori",
+    height: 1000,
+    location: "kasese",
+    type: "dormant"
+
+}
+
+//defining an empty object
+let emptyobject = {}
+//
+console.log(mountain.name);
+console.log(mountain.height);
---
+++ syntax.js
@@ -0,0 +1,39 @@
+// no variable with key words
+// no variable should start with a number 
+// no variable should start with special characters e.g @#$
+// no variable should have spaces inbetween 
+// examples
+// 1num = 20 , #num = 30(invalind naming), my num = 50
+// var let = 50 (key words so they cant be used as variable names)
+
+//valid variable naming
+//const, let, var
+//examples var num = 20; (these are statements, once they are a group, they become code)
+ /*let num2 = 60;
+ const num3 = 12; //declares a statement that is constant
+// when you give a value to a statement, its called declaring. Initialising is The process of creating a memory space in a computer.
+// use dir to check where your file is!!
+let ans = num2 + num3;
+console.log(ans);*/
+/*
+var num1 = 100;
+var name = 'Mago'; //names and words are strings so they have to be in quotes ""
+var num2 = 100.0;
+var bol = false;
+console.log(typeof(num1)); 
+*/
+const years = 30;
+switch(years) {
+  case 20:
+    console.log("I am in my 20s");
+    break;
+    case 18:
+      
+      console.log("I am a teenager");
+      break;
+      
+      case 40:
+      console.log("Damn I am old!!");
+      break;
+
+}
